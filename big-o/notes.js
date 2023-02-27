f66c60dad86bde9ddbb94e003b1846ac3fc9f4d6
davidhlee94 <davidhlee94@gmail.com>
2023-02-26 16:41:08 -0800

    big o(n)

--- big-o/notes.js
+++ big-o/notes.js
@@ -11,4 +11,27 @@ function findNemo(array){
     }
 }
 
-findNemo(large); // O(n) --> Linear Time
+findNemo(large); 
+// O(n) --> Linear Time
+// It it going through each index in the array to check if it's Nemo
+// O(10000)
+
+
+
+
+function compressFirstBox(boxes){
+    console.log(boxes[0]);
+}
+// O(1) --> Constant Time
+// It's only pulling from the first index, no matter how large the array is
+
+
+
+const boxes = [0, 1, 2, 3, 4, 5]
+function logFirstTwoBoxes(boxes){
+    console.log(boxes[0]); //O(1)
+    console.log(boxes[1]); //O(1)
+}
+
+logFirstTwoBoxes(boxes) //O(1)
+//It's not checking each index in the array. It's only checking the index of [0] and [1]
