ac7677c6c073ad66003c5059cf610b0b459f25b4
davidhlee94 <davidhlee94@gmail.com>
2023-02-27 16:49:33 -0800

    exercises

--- coding-problems/exercise1.js
+++ coding-problems/exercise1.js
@@ -12,6 +12,8 @@
 // The function has 2 parameters -arrays- no size limit
 // return true or false
 
+
+//#1 
 //O(a*b) nested loop (not the best solution)
 
 // function containsCommonItem(arr1, arr2) {
@@ -31,20 +33,38 @@
 
 
 
+//#2 
 const array1 = ['a', 'b', 'c', 'x'];
 const array2 = ['z', 'y', 'x'];
 
-//array1 ==> obj{
-// a:true,   
-// b:true,   
-// c:true,   
-// x:true,   
-//}
-//array2[index] === obj.properties
-
-function containsCommonItem2(arr1, arr2){
+function containsCommonItem2(arr1, arr2) {
     //loop through first array and create object where properties === items in the array
+    let map = {};
+    for (let i = 0; i < arr1.length; i++) {
+        if (!map[arr1[i]]) {
+            const item = arr1[i];
+            map[item] = true;
+        }
+    }
+
     //loop through second array and check if item in second array exists on created object.
+    for (let j = 0; j < arr2.length; j++) {
+        if (map[arr2[j]]) {
+            return (arr2[j] + ": " + true);
+        }
+    }
+
+    return false;
 }
 
-//O(a + b)
+//O(a + b) better time complexity
+console.log(containsCommonItem2(array1, array2))
+
+
+
+//#3
+function containsCommonItem3(arr1, arr2){
+    return arr1.some(item => arr2.includes(item))
+}
+
+console.log(containsCommonItem3(array2, array2))
