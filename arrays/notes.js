9f85127cec4a66424373957afad8a8ea877f12dd
davidhlee94 <davidhlee94@gmail.com>
2023-02-28 17:49:56 -0800

    array notes

---
+++ arrays/notes.js
@@ -0,0 +1,11 @@
+const strings = ['a', 'b', 'c', 'd']
+//4 x 4 = 16 bytes of storage
+
+
+// strings.push('e');
+// strings.pop('f');
+// strings.unshift('g')
+strings.splice(2, 0, 'alien')
+
+
+console.log(strings)
